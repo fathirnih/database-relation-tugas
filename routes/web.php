9d9714116d44511fb59230793851a37f7d92f16a
Fathir <fathirulfatih29@gmail.com>
2025-10-23 14:35:42 +0700

    add routes admin

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,18 @@ Route::get('/', function () {
 
 Route::get('/contact', function () {
     return view('contact');})->name('contact');
+
+
+    use App\Http\Controllers\AdminController;
+
+// Form login admin
+Route::get('/admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
+
+// Proses login
+Route::post('/admin/login', [AdminController::class, 'login'])->name('admin.login.submit');
+
+// Dashboard admin (hanya bisa diakses setelah login)
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard')->middleware('admin.auth');
+
+// Logout
+Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
