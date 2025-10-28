3c734c2c88f41ac30cfa8733cb54e88bd96eac23
Fathir <fathirulfatih29@gmail.com>
2025-10-28 08:37:09 +0700

    add Debug Bar

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Barryvdh\Debugbar\Facades\Debugbar;
+
 
 abstract class Controller
 {
-    //
+   
+
+public function index()
+{
+    Debugbar::info('Halo King! Debugbar aktif nih 😎');
+    Debugbar::warning('Ini peringatan!');
+    Debugbar::error('Ada error nih!');
+    Debugbar::addMessage('Pesan custom kamu di debug bar.');
+
+    return view('index');
+}
+
 }
